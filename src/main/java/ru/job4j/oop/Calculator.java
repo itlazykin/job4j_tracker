68129e08d923056ead257138a49a984092ab5c18
itlazykin <dzaka89@gmail.com>
2024-07-02 11:47:22 +0300

    6.3. Статические вложенные классы [#504951 #375319]

--- src/main/java/ru/job4j/oop/Calculator.java
+++ src/main/java/ru/job4j/oop/Calculator.java
@@ -23,6 +23,24 @@ public class Calculator {
         return sum(a) + multiply(a) + minus(a) + divide(a);
     }
 
+    public static class Multiple {
+
+        private final int result;
+
+        public Multiple(int number) {
+            result = number;
+        }
+
+        public int getResult() {
+            return result;
+        }
+    }
+
+    public static Multiple getMultiple(int value) {
+        int result = value * value;
+        return new Multiple(result);
+    }
+
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
         System.out.println(calculator.divide(10));
@@ -30,5 +48,7 @@ public class Calculator {
         System.out.println(calculator.sumAllOperation(10));
         System.out.println(sum(5));
         System.out.println(minus(2));
+        Calculator.Multiple multiple = Calculator.getMultiple(3);
+        System.out.println("Квадрат числа равен " + multiple.getResult());
     }
 }
