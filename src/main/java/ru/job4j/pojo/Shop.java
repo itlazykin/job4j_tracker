14a8478ec251d857c220d38ab1a2d5c93a1ec632
itlazykin <dzaka89@gmail.com>
2024-07-04 10:25:11 +0300

    3.1. Массив с пустыми ячейками [#176470 #250477]

---
+++ src/main/java/ru/job4j/pojo/Shop.java
@@ -0,0 +1,20 @@
+package ru.job4j.pojo;
+
+public class Shop {
+    /**
+     * Метод, который вернет индекс первой пустой ячейки.
+     *
+     * @param products списаок, в котором будем искать.
+     * @return индекс первой пустой ячейки.
+     */
+    public static int indexOfNull(Product[] products) {
+        int result = -1;
+        for (int i = 0; i < products.length; i++) {
+            if (products[i] == null) {
+                result = i;
+                break;
+            }
+        }
+        return result;
+    }
+}
