5af247d4e4ee56b5274b68620cbd64caa5cbc444
itlazykin <dzaka89@gmail.com>
2024-07-12 13:28:35 +0300

    13. Работа со строками. StringBuilder, StringJoiner, StringBuffer. [#504915 #375931]

---
+++ src/main/java/ru/job4j/concat/JoinExample.java
@@ -0,0 +1,9 @@
+package ru.job4j.concat;
+
+public class JoinExample {
+    public static void main(String[] args) {
+        String string = String.join(" ", "abc", "def", "ghi");
+        System.out.println(string);
+    }
+}
+
