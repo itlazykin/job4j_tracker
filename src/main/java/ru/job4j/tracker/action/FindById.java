cfb5070516009da2fa32d35737aa7bb9c7d26297
itlazykin <dzaka89@gmail.com>
2024-07-17 16:13:44 +0300

    Именование классов реализующих интерфейс [#505124 #484821]

--- src/main/java/ru/job4j/tracker/action/FindById.java
+++ src/main/java/ru/job4j/tracker/action/FindById.java
@@ -1,9 +1,11 @@
-package ru.job4j.tracker;
+package ru.job4j.tracker.action;
 
-public class FindByIdAction implements UserAction {
+import ru.job4j.tracker.*;
+
+public class FindById implements UserAction {
     private final Output output;
 
-    public FindByIdAction(Output output) {
+    public FindById(Output output) {
         this.output = output;
     }
 
