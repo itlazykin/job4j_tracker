392851e9616944a2324e95922bf9de82bc30ee2e
itlazykin <dzaka89@gmail.com>
2024-08-09 10:29:48 +0300

    2.3. Встроенные функциональные интерфейсы. [#249209 #366734]

---
+++ src/main/java/ru/job4j/function/StrategyUsage.java
@@ -0,0 +1,47 @@
+package ru.job4j.function;
+
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+public class StrategyUsage {
+    public String transform(Function<String, String> function, String string) {
+        return function.apply(string);
+    }
+
+    public boolean check(Predicate<String> predicate, String string) {
+        return predicate.test(string);
+    }
+
+    public static void main(String[] args) {
+        StrategyUsage usage = new StrategyUsage();
+        System.out.println("Результат работы: " + usage.check(
+                        string -> string.isEmpty(), ""
+                )
+        );
+        System.out.println(
+                "Результат работы: " + usage.check(
+                        string -> string.startsWith("Fun"), "Functional interface"
+                )
+        );
+        System.out.println(
+                "Результат работы: " + usage.check(
+                        string -> string.contains("rn"), "Surname Name"
+                )
+        );
+        System.out.println(
+                "Строка после преобразования: " + usage.transform(
+                        string -> string.toUpperCase(), "aBCdEfghKLmnpRstU"
+                )
+        );
+        System.out.println(
+                usage.transform(
+                        string -> string.concat("работает корректно."), "Строка после преобразования: "
+                )
+        );
+        System.out.println(
+                "Строка после преобразования: " + usage.transform(
+                        string -> string.trim(), "    aBC dEfghK Lmnp RstU        "
+                )
+        );
+    }
+}
