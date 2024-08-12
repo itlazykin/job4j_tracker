31e4c420b01eb58f4f2a796a35a98d10f53a30b8
itlazykin <dzaka89@gmail.com>
2024-08-12 12:20:54 +0300

    5. Фильтрация учеников [#110058 #368159]

---
+++ src/main/java/ru/job4j/stream/School.java
@@ -0,0 +1,14 @@
+package ru.job4j.stream;
+
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Stream;
+
+public class School {
+    public List<Student> collect(List<Student> students, Predicate<Student> predicate) {
+        return students.stream()
+                .flatMap(Stream::ofNullable)
+                .filter(predicate)
+                .toList();
+    }
+}
