27dcf8623c710eb1fb0dafbd218f4598958e3bed
itlazykin <dzaka89@gmail.com>
2024-11-15 10:19:48 +0300

    2. Трекер SQL [#1734 #521738]. Создание объекта new Item() вынес в приватный метод.

--- src/main/java/ru/job4j/tracker/SqlTracker.java
+++ src/main/java/ru/job4j/tracker/SqlTracker.java
@@ -19,8 +19,8 @@ public class SqlTracker implements Store {
     }
 
     private void init() {
-        try (InputStream input = SqlTracker.class.getClassLoader().getResourceAsStream(
-                "db/liquibase.properties")) {
+        try (InputStream input = SqlTracker.class.getClassLoader()
+                .getResourceAsStream("app.properties")) {
             Properties config = new Properties();
             config.load(input);
             Class.forName(config.getProperty("driver-class-name"));
@@ -34,25 +34,6 @@ public class SqlTracker implements Store {
         }
     }
 
-    private List<Item> getListOfItems(String query) {
-        List<Item> result = new ArrayList<>();
-        try (Statement statement = connection.createStatement()) {
-            ResultSet resultSet = statement.executeQuery(query);
-            while (resultSet.next()) {
-                result.add(
-                        new Item(
-                                resultSet.getInt("id"),
-                                resultSet.getString("name"),
-                                resultSet.getTimestamp("created").toLocalDateTime()
-                        )
-                );
-            }
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-        return result;
-    }
-
     @Override
     public void close() throws SQLException {
         if (connection != null) {
@@ -62,65 +43,110 @@ public class SqlTracker implements Store {
 
     @Override
     public Item add(Item item) {
-        try (PreparedStatement statement = connection.prepareStatement(
-                "INSERT INTO items(name, created) VALUES (?, ?)",
-                Statement.RETURN_GENERATED_KEYS
-        )) {
-            statement.setString(1, item.getName());
-            statement.setTimestamp(2, Timestamp.valueOf(item.getCreated()));
-            statement.execute();
-            ResultSet generatedKeys = statement.getGeneratedKeys();
-            if (generatedKeys.next()) {
-                item.setId(generatedKeys.getInt(1));
+        try (PreparedStatement preparedStatement =
+                     connection.prepareStatement("INSERT INTO items(name, created) values (?, ?)",
+                             Statement.RETURN_GENERATED_KEYS)) {
+            preparedStatement.setString(1, item.getName());
+            preparedStatement.setTimestamp(2, Timestamp.valueOf(item.getCreated()));
+            preparedStatement.execute();
+            try (ResultSet generatedKeys = preparedStatement.getGeneratedKeys()) {
+                if (generatedKeys.next()) {
+                    item.setId(generatedKeys.getInt(1));
+                }
             }
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
         return item;
     }
 
     @Override
     public boolean replace(int id, Item item) {
-        boolean result;
-        try (PreparedStatement statement = connection.prepareStatement(
-                "UPDATE items SET name = ?, created = ? WHERE id = ?"
-        )) {
-            statement.setString(1, item.getName());
-            statement.setTimestamp(2, Timestamp.valueOf(item.getCreated()));
-            statement.setInt(3, id);
-            result = statement.executeUpdate() == 1;
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
+        boolean result = findById(id) != null;
+        if (result) {
+            try (PreparedStatement preparedStatement =
+                         connection.prepareStatement("UPDATE items set name = ? where id = ?")) {
+                preparedStatement.setString(1, item.getName());
+                preparedStatement.setInt(2, id);
+                preparedStatement.execute();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
         return result;
     }
 
     @Override
     public void delete(int id) {
-        try (PreparedStatement statement = connection.prepareStatement("DELETE FROM items WHERE id = ?")) {
-            statement.setInt(1, id);
-            statement.executeUpdate();
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
+        if (findById(id) != null) {
+            try (PreparedStatement preparedStatement =
+                         connection.prepareStatement("DELETE FROM items WHERE id = ?")) {
+                preparedStatement.setInt(1, id);
+                preparedStatement.execute();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
+
     }
 
     @Override
     public List<Item> findAll() {
-        return getListOfItems("SELECT * FROM items");
+        List<Item> result = new ArrayList<>();
+        try (PreparedStatement preparedStatement =
+                     connection.prepareStatement("SELECT * FROM items")) {
+            ResultSet rs = preparedStatement.executeQuery();
+            while (rs.next()) {
+                result.add(createItem(rs));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
     }
 
     @Override
     public List<Item> findByName(String key) {
-        return getListOfItems(String.format("SELECT * FROM items WHERE name = '%s'", key));
+        List<Item> result = new ArrayList<>();
+        try (PreparedStatement preparedStatement =
+                     connection.prepareStatement("SELECT * FROM items WHERE name = ?")) {
+            preparedStatement.setString(1, key);
+            ResultSet rs = preparedStatement.executeQuery();
+            while (rs.next()) {
+                result.add(createItem(rs));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
     }
 
     @Override
     public Item findById(int id) {
+        Item item = null;
+        try (PreparedStatement preparedStatement =
+                     connection.prepareStatement("SELECT * FROM items WHERE id = ?"
+                     )) {
+            preparedStatement.setInt(1, id);
+            ResultSet rs = preparedStatement.executeQuery();
+            while (rs.next()) {
+                item = createItem(rs);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return item;
+    }
+
+    private Item createItem(ResultSet rs) {
         Item result = null;
-        List<Item> list = getListOfItems(String.format("SELECT * FROM items WHERE id = %d", id));
-        if (!list.isEmpty()) {
-            result = list.get(0);
+        try {
+            result = new Item(rs.getInt(1),
+                    rs.getString(2),
+                    rs.getTimestamp(3).toLocalDateTime()
+            );
+        } catch (Exception e) {
+            e.printStackTrace();
         }
         return result;
     }
